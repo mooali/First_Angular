1a2a5f9e7273f798ed6052cb783504b65bf79cc3
Mohammed Ali <mohammed.ali@students.bfh.ch>
2021-01-16 21:08:35 +0100

    Second task, v01.0 tabs changing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import {MatInputModule} from '@angular/material/input';
-
-
 import { AppComponent } from './app.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -15,9 +13,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCheckboxModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
