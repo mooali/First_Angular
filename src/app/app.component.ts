19a5d828e707c4c1803b12cac83f7799157dcdbb
Mohammed Ali <mohammed.ali@students.bfh.ch>
2021-01-17 14:26:58 +0100

    added some comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ import {Todo} from './models/Todo';
 })
 export class AppComponent {
   title = 'write something in the input field to change this label';
+  /**
+   * accept only Alphanumeric input and spaces
+   */
   textValidate = new FormControl('', [Validators.pattern('^[a-zA-Z0-9 ]*$')]);
   todos: Todo[] = [
     {
@@ -33,15 +36,30 @@ export class AppComponent {
     },
   ];
 
+
+  /**
+   * this will update the label based on the user input.
+   * if the label is empty, lebel will reset on it's initial title
+   * @param event
+   */
   // tslint:disable-next-line:typedef
   updateLabel(event: any) {
     event.target.value ? this.title = event.target.value : this.title = 'write something in the input field to change this label';
   }
 
+  /**
+   * reset the label to it's initial message.
+   * this is used after changing tabs.
+   */
   // tslint:disable-next-line:typedef
   resetLabel() {
     this.title = this.title = 'write something in the input field to change this label';
   }
+
+  /**
+   * if the checkboxs is checked, task is completed.
+   * @param id of the task(Todo task)
+   */
   // tslint:disable-next-line:typedef
   onChange(id: number) {
     for (const todo of this.todos){
